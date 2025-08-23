9193f6c37d0829b3648adceb81747c6c53ce649c
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2025-08-23 21:50:08 +0530

    assignment72.1done

--- assignment72.1/src/components/Navbar.jsx
+++ assignment72.1/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center gap-1 py-4 px-8 border-b border-gray-200'>
         <LuListTodo className='text-4xl text-yellow-500' />
-        <h1 className='text-xl font-bold'>TaskZen</h1>
+        <h1 className='text-xl font-bold'>TODO</h1>
     </div>
   )
 }
